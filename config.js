7220e223b804a5a0d9d3a7840d166955deb8e8f9
adam nunez <adam.a.nunez@gmail.com>
2024-05-09 21:49:55 -0700

    Style change, remove semicolons

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
 $(document).ready(() => {
     console.log("Loaded project portal config")
-    const prefix = ExternalModules.UWMadison.ProjectPortal.prefix;
-    const helperButtons = ExternalModules.UWMadison.ProjectPortal.helperButtons;
-    const url = window.location.href.split('/ExternalModules')[0];
-    const $modal = $('#external-modules-configure-modal');
+    const prefix = ExternalModules.UWMadison.ProjectPortal.prefix
+    const helperButtons = ExternalModules.UWMadison.ProjectPortal.helperButtons
+    const url = window.location.href.split('/ExternalModules')[0]
+    const $modal = $('#external-modules-configure-modal')
     $modal.on('show.bs.modal', (el) => {
         // Making sure we are overriding our modules's modal only.
-        if ($(el.currentTarget).data('module') !== prefix) return;
+        if ($(el.currentTarget).data('module') !== prefix) return
 
         if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld === 'undefined')
-            ExternalModules.Settings.prototype.resetConfigInstancesOld = ExternalModules.Settings.prototype.resetConfigInstances;
+            ExternalModules.Settings.prototype.resetConfigInstancesOld = ExternalModules.Settings.prototype.resetConfigInstances
 
         ExternalModules.Settings.prototype.resetConfigInstances = function () {
-            ExternalModules.Settings.prototype.resetConfigInstancesOld();
-            if ($modal.data('module') !== prefix) return;
+            ExternalModules.Settings.prototype.resetConfigInstancesOld()
+            if ($modal.data('module') !== prefix) return
 
-            $modal.find(".external-modules-input-td").css('width', '250px');
-            $modal.find('thead').remove();
-            $modal.find("tr").not(".sub_start").find("span.external-modules-instance-label").remove();
+            $modal.find(".external-modules-input-td").css('width', '250px')
+            $modal.find('thead').remove()
+            $modal.find("tr").not(".sub_start").find("span.external-modules-instance-label").remove()
 
             // Make URL wide
             $modal.find("tr[field=destination]").each((_, el) => {
-                let $tr = $(el);
+                let $tr = $(el)
                 if ($tr.find('td').length == 3) {
-                    $tr.find('td').first().remove();
-                    let a = $tr.find('input').prop('name').split('____')[1];
+                    $tr.find('td').first().remove()
+                    let a = $tr.find('input').prop('name').split('____')[1]
                     $tr.find('td').first().attr('colspan', '2').prepend(
                         `<b>Destination URL:</b><br>
                         <span id="isRedcap___${a}">${url}</span><br>`
@@ -35,81 +35,81 @@ $(document).ready(() => {
                         <span style="margin-left:104px"> Get a YMD date with N working days offset using [work+N] </span><br>
                         <span style="margin-left:104px"> Event IDs can be piped via [eventname_arm_1] or [event-id] for the current event </span><br>
                         <span style="margin-left:104px"> The current URL can be piped via [current-url] </span>`
-                    );
-                    $tr.find('input').addClass("mt-1");
+                    )
+                    $tr.find('input').addClass("mt-1")
                 }
-            });
+            })
 
             // Default and branching logic in EMs doesn't work at time of writing
             // Hide values for redcap specific stuff
             $("input[name^=isredcap_]").on('click', (el) => {
-                let $btn = $(el.currentTarget);
-                let a = $btn.prop('name').split('____')[1];
+                let $btn = $(el.currentTarget)
+                let a = $btn.prop('name').split('____')[1]
                 if ($btn.val() == "1") {
-                    $(`input[name=isrepeating____${a}]`).closest('tr').show();
-                    $(`#isRedcap___${a}`).show();
+                    $(`input[name=isrepeating____${a}]`).closest('tr').show()
+                    $(`#isRedcap___${a}`).show()
                     if ($(`input[name=isrepeating____${a}]:checked`).val() == "1")
-                        $(`#isRepeating___${a}`).show();
+                        $(`#isRepeating___${a}`).show()
                 }
                 else {
-                    $(`input[name=isrepeating____${a}]`).closest('tr').hide();
-                    $(`input[name=redcap-hide____${a}]`).closest('tr').hide();
-                    $(`#isRedcap___${a}`).hide();
-                    $(`#isRepeating___${a}`).hide();
+                    $(`input[name=isrepeating____${a}]`).closest('tr').hide()
+                    $(`input[name=redcap-hide____${a}]`).closest('tr').hide()
+                    $(`#isRedcap___${a}`).hide()
+                    $(`#isRepeating___${a}`).hide()
                 }
-            });
+            })
 
             // Hide repeating instrument span
             $("input[name^=isrepeating_]").on('click', (el) => {
-                let $btn = $(el.currentTarget);
-                let a = $btn.prop('name').split('____')[1];
+                let $btn = $(el.currentTarget)
+                let a = $btn.prop('name').split('____')[1]
                 if ($btn.val() == "1") {
-                    $(`#isRepeating___${a}`).show();
+                    $(`#isRepeating___${a}`).show()
                 }
                 else {
-                    $(`#isRepeating___${a}`).hide();
+                    $(`#isRepeating___${a}`).hide()
                 }
-            });
+            })
 
             // Hide values for modal specific stuff
             $("input[name^=inmodal]").on('click', (el) => {
-                let $btn = $(el.currentTarget);
-                let a = $btn.prop('name').split('____')[1];
+                let $btn = $(el.currentTarget)
+                let a = $btn.prop('name').split('____')[1]
                 if ($btn.val() == "1") {
-                    $(`input[name=modal-height____${a}]`).closest('tr').show();
-                    $(`input[name=modal-width____${a}]`).closest('tr').show();
-                    $(`input[name=hide-close-button____${a}]`).closest('tr').show();
-                    $(`input[name=redcap-hide____${a}]`).closest('tr').hide();
+                    $(`input[name=modal-height____${a}]`).closest('tr').show()
+                    $(`input[name=modal-width____${a}]`).closest('tr').show()
+                    $(`input[name=hide-close-button____${a}]`).closest('tr').show()
+                    $(`input[name=redcap-hide____${a}]`).closest('tr').hide()
                     if ($(`input[name=isredcap____${a}]:checked`).val() == "1")
-                        $(`input[name=redcap-hide____${a}]`).closest('tr').show();
+                        $(`input[name=redcap-hide____${a}]`).closest('tr').show()
                 }
                 else {
-                    $(`input[name=modal-height____${a}]`).closest('tr').hide();
-                    $(`input[name=modal-width____${a}]`).closest('tr').hide();
-                    $(`input[name=redcap-hide____${a}]`).closest('tr').hide();
-                    $(`input[name=hide-close-button____${a}]`).closest('tr').hide();
+                    $(`input[name=modal-height____${a}]`).closest('tr').hide()
+                    $(`input[name=modal-width____${a}]`).closest('tr').hide()
+                    $(`input[name=redcap-hide____${a}]`).closest('tr').hide()
+                    $(`input[name=hide-close-button____${a}]`).closest('tr').hide()
                 }
-            });
+            })
 
             // Default the radio value correctly
             $("input[name^=isredcap_]").each((index, el) => {
-                if (index % 2 != 0) return;
+                if (index % 2 != 0) return
                 if (!$(el).is(":checked") && !$(el).siblings("input").first().is(":checked")) {
-                    $(el).click();
-                    let a = $(el).prop('name').split('____')[1];
-                    $(`input[name=isrepeating____${a}]`).first().click();
-                    $(`input[name=redcap-hide____${a}]`).first().click();
-                    $(`input[name=inmodal____${a}]`).first().click();
+                    $(el).click()
+                    let a = $(el).prop('name').split('____')[1]
+                    $(`input[name=isrepeating____${a}]`).first().click()
+                    $(`input[name=redcap-hide____${a}]`).first().click()
+                    $(`input[name=inmodal____${a}]`).first().click()
                 }
-            });
-            $("input[type=radio]:checked").click();
-        };
-    });
+            })
+            $("input[type=radio]:checked").click()
+        }
+    })
 
     $modal.on('hide.bs.modal', (el) => {
         // Making sure we are overriding our modules's modal only.
-        if ($(el.currentTarget).data('module') !== prefix) return;
+        if ($(el.currentTarget).data('module') !== prefix) return
         if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld !== 'undefined')
-            ExternalModules.Settings.prototype.resetConfigInstances = ExternalModules.Settings.prototype.resetConfigInstancesOld;
-    });
-});
+            ExternalModules.Settings.prototype.resetConfigInstances = ExternalModules.Settings.prototype.resetConfigInstancesOld
+    })
+})
--- portal.js
+++ portal.js
@@ -5,17 +5,17 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close fullscreen" style="font-size:1.3rem" data-bs-toggle="false">
-                        <span>&#x26F6;</span>
+                        <span>&#x26F6</span>
                     </button>
                     <button type="button" class="close refresh ml-0" style="font-size:1.3rem">
-                        <span>&#8635;</span>
+                        <span>&#8635</span>
                     </button>
                     <button type="button" class="close ml-0" data-bs-dismiss="modal">
-                        <span>&times;</span>
+                        <span>&times</span>
                     </button>
                 </div>
-                <div class="modal-body" style="padding:0;display:flex;justify-content:center;">
-                    <div class="iframeLoading" style="align-self:center;color:grey;font-size:16px;">Loading...</div>
+                <div class="modal-body" style="padding:0display:flexjustify-content:center">
+                    <div class="iframeLoading" style="align-self:centercolor:greyfont-size:16px">Loading...</div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
@@ -23,229 +23,229 @@
                 </div>
             </div>
         </div>
-    </div>`;
+    </div>`
 
     const tr = () => `
     <tr id="ProjectPortal-tr" sq_id="ProjectPortal">
-        <td class="labelrc col-12" colspan="2" style="padding:0;border:0">
+        <td class="labelrc col-12" colspan="2" style="padding:0border:0">
         </td>
-    </tr>`;
+    </tr>`
 
-    const iframe = (link) => `<iframe style="display:none;width:100%;border:none" src="${link}"></iframe>`;
+    const iframe = (link) => `<iframe style="display:nonewidth:100%border:none" src="${link}"></iframe>`
 
-    const em = ExternalModules.UWMadison.ProjectPortal;
-    const insideModal = window.self !== window.top;
-    let widthCache = 'auto';
-    let loaded = false;
+    const em = ExternalModules.UWMadison.ProjectPortal
+    const insideModal = window.self !== window.top
+    let widthCache = 'auto'
+    let loaded = false
 
     $(document).ready(() => {
-        $('form tr').last().after(tr());
+        $('form tr').last().after(tr())
 
         if (require_change_reason && insideModal) {
             Swal.fire({
                 icon: 'error',
                 title: 'Incompatible Project',
                 text: 'Project portal cannot function when using "change reasons" on the target project. Please disable this feature.',
-            });
+            })
         }
 
         if (typeof Shazam !== "object") {
-            loadPortals();
-            return;
+            loadPortals()
+            return
         }
 
-        let oldCallback = Shazam.beforeDisplayCallback;
+        let oldCallback = Shazam.beforeDisplayCallback
         Shazam.beforeDisplayCallback = () => {
             if (typeof oldCallback == "function")
-                oldCallback();
-            loadPortals();
+                oldCallback()
+            loadPortals()
         }
-        setTimeout(loadPortals, 1000);
-    });
+        setTimeout(loadPortals, 1000)
+    })
 
     const loadPortals = () => {
-        if (loaded) return;
-        loaded = true;
+        if (loaded) return
+        loaded = true
 
         // Prevent google chrome bug with hidden forms
         $(window).bind('beforeunload', () => {
             if ($('.modal').is(':visible') === false)
-                $(".modal").remove();
-        });
+                $(".modal").remove()
+        })
 
         $.each(em.config, (name, config) => {
-            if ($(`.${name}`).length == 0) return;
-            $(`.${name}`).off(); // Remove all Redcap events
-            config.url = customPipes(config.url);
+            if ($(`.${name}`).length == 0) return
+            $(`.${name}`).off() // Remove all Redcap events
+            config.url = customPipes(config.url)
 
             // Not a modal, just a boring link
             if (!config.modal) {
-                $(`.${name}`).attr('href', config.url);
+                $(`.${name}`).attr('href', config.url)
                 $(`.${name}`).on('click', function () {
-                    window.location = $(this).attr('href');
-                });
-                return;
+                    window.location = $(this).attr('href')
+                })
+                return
             }
 
             // Stop that pop-up from happening
-            if (insideModal) require_change_reason = 0;
+            if (insideModal) require_change_reason = 0
 
             // Insert and modify the modal HTML
-            $("#ProjectPortal-tr td").append(modal(name));
+            $("#ProjectPortal-tr td").append(modal(name))
             if (config.hideClose)
-                $(`#${name} .btn-danger`).hide();
+                $(`#${name} .btn-danger`).hide()
             if (config.hideSave)
-                $(`#${name} .btn-success`).hide();
+                $(`#${name} .btn-success`).hide()
             $(`#${name}`).modal({
                 show: false,
                 backdrop: 'static'
-            });
+            })
 
             // Setup our click and events
-            $(`.${name}`).on('click', () => $(`#${name}`).modal('show'));
-            $(`#${name}`).on('shown.bs.modal', () => $(".modal-open").css("overflow-y", 'auto'));
-            $(`#${name}`).on('show.bs.modal', () => showModal(name, config));
-        });
+            $(`.${name}`).on('click', () => $(`#${name}`).modal('show'))
+            $(`#${name}`).on('shown.bs.modal', () => $(".modal-open").css("overflow-y", 'auto'))
+            $(`#${name}`).on('show.bs.modal', () => showModal(name, config))
+        })
     }
 
     const showModal = (name, config) => {
-        const $modal = $(`#${name}`);
-        $modal.find('.modal-dialog').css('max-width', parseCSS(config.width, $modal));
+        const $modal = $(`#${name}`)
+        $modal.find('.modal-dialog').css('max-width', parseCSS(config.width, $modal))
         $(window).on('resize', () => {
-            let t = $(`#${name} .fullscreen`).data('toggle');
-            let h = t ? window.innerHeight : parseCSS(config.height);
-            $(`#${name} .modal-dialog`).css('margin-top', t ? 0 : '1.75rem');
-            $(`#${name} .modal-dialog`).css('margin-bottom', t ? 0 : '1.75rem');
-            $(`#${name} .modal-content`).css('height', h);
-        });
-        $(window).resize();
+            let t = $(`#${name} .fullscreen`).data('toggle')
+            let h = t ? window.innerHeight : parseCSS(config.height)
+            $(`#${name} .modal-dialog`).css('margin-top', t ? 0 : '1.75rem')
+            $(`#${name} .modal-dialog`).css('margin-bottom', t ? 0 : '1.75rem')
+            $(`#${name} .modal-content`).css('height', h)
+        })
+        $(window).resize()
 
-        if ($modal.find('iframe').length != 0) return;
+        if ($modal.find('iframe').length != 0) return
 
         $modal.find('.refresh').on('click', function () {
-            $(`#${name} iframe`).get(0).contentWindow.location.reload();
-        });
+            $(`#${name} iframe`).get(0).contentWindow.location.reload()
+        })
 
         $modal.find('.fullscreen').on('click', () => {
-            let t = $(`#${name} .fullscreen`).data('toggle');
-            $(`#${name} .fullscreen`).data('toggle', !t);
+            let t = $(`#${name} .fullscreen`).data('toggle')
+            $(`#${name} .fullscreen`).data('toggle', !t)
             if (t) {
-                widthCache = $(`#${name} .modal-dialog`).width();
+                widthCache = $(`#${name} .modal-dialog`).width()
                 if (config.hide != 'all')//Only change width if we aren't showing a normal form
-                    $(`#${name} .modal-dialog`).css('max-width', `${window.innerWidth}px`);
+                    $(`#${name} .modal-dialog`).css('max-width', `${window.innerWidth}px`)
             } else {
-                $(`#${name} .modal-dialog`).css('max-width', parseCSS(config.width, $(`#${name}`)));
+                $(`#${name} .modal-dialog`).css('max-width', parseCSS(config.width, $(`#${name}`)))
             }
-            $(`#${name} .modal-dialog`).css('width', t ? "auto" : widthCache);
-            $(window).resize();
-        });
+            $(`#${name} .modal-dialog`).css('width', t ? "auto" : widthCache)
+            $(window).resize()
+        })
 
-        $modal.find('.modal-body').append(iframe(config.url));
+        $modal.find('.modal-body').append(iframe(config.url))
 
         $modal.find('iframe').on('load', function () {
-            let content = $(this).contents();
-            if ($(content).find("#header").text() == "Server Error") return;
+            let content = $(this).contents()
+            if ($(content).find("#header").text() == "Server Error") return
             if (config.hide == 'all') {
-                $(content).find('body > :not(#form)').not('.ui-dialog').hide();
-                $(content).find('#form').appendTo($(content).find('body'));
-                $(content).find('tr[id$=__-tr]').hide();
-                $(content).find('html').css('overflow-x', 'hidden');
+                $(content).find('body > :not(#form)').not('.ui-dialog').hide()
+                $(content).find('#form').appendTo($(content).find('body'))
+                $(content).find('tr[id$=__-tr]').hide()
+                $(content).find('html').css('overflow-x', 'hidden')
             } else if (config.hide == 'nav') {
-                $(content).find("#west, #south, #subheader, #fade").remove();
+                $(content).find("#west, #south, #subheader, #fade").remove()
             }
-            $(`#${name} .iframeLoading`).hide();
-            $(this).show();
-            enableRedcapSaveButtons();
-            $(content).find('input.rc-autocomplete').css('width', 'auto');
+            $(`#${name} .iframeLoading`).hide()
+            $(this).show()
+            enableRedcapSaveButtons()
+            $(content).find('input.rc-autocomplete').css('width', 'auto')
             setTimeout(() => {
-                $(`#${name}`).find('.modal-dialog').css('max-width', parseCSS(config.width, $(`#${name}`)));
-            }, 500);
-        });
+                $(`#${name}`).find('.modal-dialog').css('max-width', parseCSS(config.width, $(`#${name}`)))
+            }, 500)
+        })
 
         $modal.find('.saveButton').on('click', function () {
-            let content = $(`#${name} iframe`).contents();
+            let content = $(`#${name} iframe`).contents()
 
             // Check for required feilds
-            let reqFieldMissing = false;
+            let reqFieldMissing = false
             $(content).find('*[req]:visible').each(function () {
-                if (reqFieldMissing) return false;
-                let $input = $(this).find('select,input,textarea');
+                if (reqFieldMissing) return false
+                let $input = $(this).find('select,input,textarea')
                 if ($input.length == 1) // Select, input, or textarea 
-                    reqFieldMissing = $input.val() == "";
+                    reqFieldMissing = $input.val() == ""
                 else // checkbox
-                    reqFieldMissing = $input.not('*[id]').get().map(x => x.value).every(x => !x);
-            });
+                    reqFieldMissing = $input.not('*[id]').get().map(x => x.value).every(x => !x)
+            })
             if (reqFieldMissing) {
                 Swal.fire({
                     icon: 'error',
                     title: 'Missing Required Fields',
                     text: 'You did not provide a value for some fields that require a value. Please enter a value for the fields marked as required on this page.',
-                });
-                return;
+                })
+                return
             }
 
-            disableRedcapSaveButtons();
-            $(`#${name}`).modal('hide');
+            disableRedcapSaveButtons()
+            $(`#${name}`).modal('hide')
             if ($(content).find("#saveButton, .saveButton").not('.modal #saveButton, .modal .saveButton').length != 0) {
-                $(`#${name} iframe`).get(0).contentWindow.jQuery("#saveButton, .saveButton").not('.modal #saveButton, .modal .saveButton').click();
+                $(`#${name} iframe`).get(0).contentWindow.jQuery("#saveButton, .saveButton").not('.modal #saveButton, .modal .saveButton').click()
             } else {
-                $(content).find("#submit-btn-savecontinue").click();
+                $(content).find("#submit-btn-savecontinue").click()
             }
-        });
+        })
     }
 
     const enableRedcapSaveButtons = () => {
-        $("#__SUBMITBUTTONS__-tr button").css('pointer-events', '');
-        $(".modalTempDisableSave").last().remove();
+        $("#__SUBMITBUTTONS__-tr button").css('pointer-events', '')
+        $(".modalTempDisableSave").last().remove()
     }
 
     const disableRedcapSaveButtons = () => {
-        $("#__SUBMITBUTTONS__-tr button").css('pointer-events', 'none');
+        $("#__SUBMITBUTTONS__-tr button").css('pointer-events', 'none')
         if ($(".modalTempDisableSave").length > 0)
             $("#questiontable").after(`<div class='modalTempDisableSave d-none'></div>`)
         else
             $("#__SUBMITBUTTONS__-tr button").last().after(`<span class='text-bold text-danger modalTempDisableSave'><br>* Form saving disabled while modal is saved</span>`)
-        setTimeout(enableRedcapSaveButtons, 3000);
+        setTimeout(enableRedcapSaveButtons, 3000)
     }
 
     const customPipes = (input) => {
-        const ymd = 'y-MM-dd';
+        const ymd = 'y-MM-dd'
         const config = {
             "today": (date, days) => {
-                let result = new Date(date);
-                result.setDate(result.getDate() + days);
-                return result;
+                let result = new Date(date)
+                result.setDate(result.getDate() + days)
+                return result
             },
             "work": (date, days) => {
-                let result = new Date(date);
+                let result = new Date(date)
                 result.setDate(result.getDate() + days + (result.getDay() === 6 ? 2 : +!result.getDay()) +
                     (Math.floor((days - 1 + (result.getDay() % 6 || 1)) / 5) * 2))
-                return result;
+                return result
             }
-        };
-        input = input.replaceAll("[current-url]", encodeURIComponent(window.location.href));
-        input = input.replaceAll("[today]", formatDate(new Date(), ymd));
+        }
+        input = input.replaceAll("[current-url]", encodeURIComponent(window.location.href))
+        input = input.replaceAll("[today]", formatDate(new Date(), ymd))
         $.each(config, (pipeName, func) => {
-            if (!input.includes(`[${pipeName}`)) return;
-            let pipes = input.match(new RegExp(`(?:${pipeName}){1}([+-]([0-9]*))+`, 'g'));
+            if (!input.includes(`[${pipeName}`)) return
+            let pipes = input.match(new RegExp(`(?:${pipeName}){1}([+-]([0-9]*))+`, 'g'))
             pipes.forEach((pipe) => {
-                if (!pipe) return;
-                let number = parseInt(pipe.split(pipeName)[1]);
-                let text = formatDate(func(new Date(), number), ymd);
-                input = input.replace(`[${pipe}]`, text);
-            });
+                if (!pipe) return
+                let number = parseInt(pipe.split(pipeName)[1])
+                let text = formatDate(func(new Date(), number), ymd)
+                input = input.replace(`[${pipe}]`, text)
+            })
         })
-        return input;
+        return input
     }
 
     const parseCSS = (setting, $modalWidth) => {
         if (!setting && $modalWidth) {
-            const scrollable = $modalWidth.find('iframe').contents().height() > ($modalWidth.find('iframe').height() + 1);
-            return scrollable ? '822px' : '806px'; // default blank Widths
+            const scrollable = $modalWidth.find('iframe').contents().height() > ($modalWidth.find('iframe').height() + 1)
+            return scrollable ? '822px' : '806px' // default blank Widths
         }
-        setting = setting || '90%'; // Default blank Height
+        setting = setting || '90%' // Default blank Height
         if (setting.includes('%'))
-            return Math.floor(($modalWidth ? window.innerWidth : window.innerHeight) * (parseInt(setting.replace('%', '')) / 100)) + "px";
-        return setting;
+            return Math.floor(($modalWidth ? window.innerWidth : window.innerHeight) * (parseInt(setting.replace('%', '')) / 100)) + "px"
+        return setting
     }
-})();
+})()
